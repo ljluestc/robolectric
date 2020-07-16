710465a0a93aa051b44b53d2dfff51163ff3fadc
Googler <noreply@google.com>
2020-07-16 07:43:05 -0700

    Add ability to query nanoapps from the ShadowContextHubManager.
    PiperOrigin-RevId: 321563530

--- robolectric/src/test/java/org/robolectric/shadows/ShadowContextHubManagerTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowContextHubManagerTest.java
@@ -1,11 +1,15 @@
 package org.robolectric.shadows;
 
 import static com.google.common.truth.Truth.assertThat;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import android.content.Context;
 import android.hardware.location.ContextHubClient;
 import android.hardware.location.ContextHubInfo;
 import android.hardware.location.ContextHubManager;
+import android.hardware.location.ContextHubTransaction;
+import android.hardware.location.NanoAppInstanceInfo;
+import android.hardware.location.NanoAppState;
 import android.os.Build;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -17,7 +21,7 @@ import org.robolectric.annotation.Config;
 
 /** Tests for {@link ShadowContextHubManager}. */
 @RunWith(AndroidJUnit4.class)
-@Config(minSdk = Build.VERSION_CODES.P)
+@Config(minSdk = Build.VERSION_CODES.N)
 public class ShadowContextHubManagerTest {
   // Do not reference a non-public field in a test, because those get loaded outside the Robolectric
   // sandbox
@@ -31,6 +35,7 @@ public class ShadowContextHubManagerTest {
   }
 
   @Test
+  @Config(minSdk = Build.VERSION_CODES.P)
   public void getContextHubs_returnsValidList() {
     ContextHubManager contextHubManager =
         (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
@@ -40,10 +45,115 @@ public class ShadowContextHubManagerTest {
   }
 
   @Test
+  @Config(minSdk = Build.VERSION_CODES.P)
   public void createClient_returnsValidClient() {
     ContextHubManager contextHubManager =
         (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
     ContextHubClient contextHubClient = contextHubManager.createClient(null, null);
     assertThat(contextHubClient).isNotNull();
+  }
+
+  @Test
+  @Config(minSdk = Build.VERSION_CODES.P)
+  public void queryNanoApps_returnsValidNanoapps() throws Exception {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    List<ContextHubInfo> contextHubInfoList = contextHubManager.getContextHubs();
+    long nanoappId = 5;
+    int nanoappVersion = 1;
+    ShadowContextHubManager.addNanoapp(
+        contextHubInfoList.get(0), 0 /* nanoappUid */, nanoappId, nanoappVersion);
+
+    ContextHubTransaction<List<NanoAppState>> transaction =
+        contextHubManager.queryNanoApps(contextHubInfoList.get(0));
+
+    assertThat(transaction.getType()).isEqualTo(ContextHubTransaction.TYPE_QUERY_NANOAPPS);
+    ContextHubTransaction.Response<List<NanoAppState>> response =
+        transaction.waitForResponse(1, SECONDS);
+    assertThat(response.getResult()).isEqualTo(ContextHubTransaction.RESULT_SUCCESS);
+    List<NanoAppState> states = response.getContents();
+    assertThat(states).isNotNull();
+    assertThat(states).hasSize(1);
+    NanoAppState state = states.get(0);
+    assertThat(state.getNanoAppId()).isEqualTo(nanoappId);
+    assertThat(state.getNanoAppVersion()).isEqualTo(nanoappVersion);
+    assertThat(state.isEnabled()).isTrue();
+  }
+
+  @Test
+  @Config(minSdk = Build.VERSION_CODES.P)
+  public void queryNanoApps_noNanoappsAdded() throws Exception {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    List<ContextHubInfo> contextHubInfoList = contextHubManager.getContextHubs();
+
+    ContextHubTransaction<List<NanoAppState>> transaction =
+        contextHubManager.queryNanoApps(contextHubInfoList.get(0));
+
+    assertThat(transaction.getType()).isEqualTo(ContextHubTransaction.TYPE_QUERY_NANOAPPS);
+    ContextHubTransaction.Response<List<NanoAppState>> response =
+        transaction.waitForResponse(1, SECONDS);
+    assertThat(response.getResult()).isEqualTo(ContextHubTransaction.RESULT_SUCCESS);
+    List<NanoAppState> states = response.getContents();
+    assertThat(states).isNotNull();
+    assertThat(states).isEmpty();
+  }
+
+  @Test
+  public void getContextHubHandles_returnsValidArray() {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    int[] handles = contextHubManager.getContextHubHandles();
+    assertThat(handles).isNotNull();
+    assertThat(handles).isNotEmpty();
+  }
+
+  @Test
+  public void getContextHubInfo_returnsValidInfo() {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    int[] handles = contextHubManager.getContextHubHandles();
+    assertThat(handles).isNotNull();
+    for (int handle : handles) {
+      assertThat(contextHubManager.getContextHubInfo(handle)).isNotNull();
+    }
+  }
+
+  @Test
+  public void getContextHubInfo_returnsInvalidInfo() {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    int[] handles = contextHubManager.getContextHubHandles();
+    assertThat(handles).isNotNull();
+    assertThat(contextHubManager.getContextHubInfo(-1)).isNull();
+    assertThat(contextHubManager.getContextHubInfo(handles.length)).isNull();
+  }
+
+  @Test
+  public void getNanoAppInstanceInfo_returnsValidInfo() {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+    int[] handles = contextHubManager.getContextHubHandles();
+    ContextHubInfo hubInfo = contextHubManager.getContextHubInfo(handles[0]);
+    long nanoappId = 5;
+    int nanoappVersion = 1;
+    int nanoappUid = 0;
+    ShadowContextHubManager.addNanoapp(hubInfo, nanoappUid, nanoappId, nanoappVersion);
+
+    NanoAppInstanceInfo info = contextHubManager.getNanoAppInstanceInfo(nanoappUid);
+
+    assertThat(info).isNotNull();
+    assertThat(info.getAppId()).isEqualTo(nanoappId);
+    assertThat(info.getAppVersion()).isEqualTo(nanoappVersion);
+  }
+
+  @Test
+  public void getNanoAppInstanceInfo_noNanoappsAdded() {
+    ContextHubManager contextHubManager =
+        (ContextHubManager) context.getSystemService(Context.CONTEXTHUB_SERVICE);
+
+    NanoAppInstanceInfo info = contextHubManager.getNanoAppInstanceInfo(0 /* nanoappUid */);
+
+    assertThat(info).isNull();
   }
 }
--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextHubManager.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowContextHubManager.java
@@ -1,29 +1,86 @@
 package org.robolectric.shadows;
 
 import android.hardware.location.ContextHubClient;
-import android.hardware.location.ContextHubClientCallback;
 import android.hardware.location.ContextHubInfo;
 import android.hardware.location.ContextHubManager;
+import android.hardware.location.ContextHubTransaction;
+import android.hardware.location.NanoAppInstanceInfo;
+import android.hardware.location.NanoAppState;
+import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.Executor;
+import java.util.Map;
+import java.util.Set;
 import org.robolectric.annotation.HiddenApi;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.Resetter;
 import org.robolectric.util.ReflectionHelpers;
+import org.robolectric.util.ReflectionHelpers.ClassParameter;
 
 /** Shadow for {@link ContextHubManager}. */
 @Implements(
     value = ContextHubManager.class,
-    minSdk = VERSION_CODES.P,
+    minSdk = VERSION_CODES.N,
     isInAndroidSdk = false,
     looseSignatures = true)
 public class ShadowContextHubManager {
   private static final List<ContextHubInfo> contextHubInfoList = new ArrayList<>();
+  private static final Map<Integer, NanoAppInstanceInfo> nanoappUidToInfo = new HashMap<>();
+  private static final Map<ContextHubInfo, Set<Integer>> contextHubToNanoappUid = new HashMap<>();
 
   static {
     contextHubInfoList.add(new ContextHubInfo());
+  }
+
+  @Resetter
+  public static void reset() {
+    nanoappUidToInfo.clear();
+    contextHubToNanoappUid.clear();
+  }
+
+  /** Adds a nanoapp to the list of nanoapps that are supported by the provided contexthubinfo. */
+  public static void addNanoapp(
+      ContextHubInfo info, int nanoappUid, long nanoappId, int nanoappVersion) {
+    NanoAppInstanceInfo instanceInfo;
+    if (VERSION.SDK_INT >= VERSION_CODES.P) {
+      instanceInfo = createInstanceInfoP(info, nanoappUid, nanoappId, nanoappVersion);
+    } else {
+      instanceInfo = createInstanceInfoPreP(nanoappId, nanoappVersion);
+    }
+
+    Set<Integer> nanoappUids = contextHubToNanoappUid.get(info);
+    if (nanoappUids == null) {
+      nanoappUids = new HashSet<>();
+    }
+    nanoappUids.add(nanoappUid);
+    contextHubToNanoappUid.put(info, nanoappUids);
+    nanoappUidToInfo.put(nanoappUid, instanceInfo);
+  }
+
+  /** Creates a {@link NanoAppInstanceInfo} based on APIs available in P and later. */
+  public static NanoAppInstanceInfo createInstanceInfoP(
+      ContextHubInfo info, int nanoappUid, long nanoappId, int nanoappVersion) {
+    return new NanoAppInstanceInfo(nanoappUid, nanoappId, nanoappVersion, info.getId());
+  }
+
+  /** Creates a {@link NanoAppInstanceInfo} based on APIs available prior to P. */
+  public static NanoAppInstanceInfo createInstanceInfoPreP(long nanoappId, int nanoappVersion) {
+    NanoAppInstanceInfo instanceInfo = new NanoAppInstanceInfo();
+    ReflectionHelpers.callInstanceMethod(
+        NanoAppInstanceInfo.class,
+        instanceInfo,
+        "setAppId",
+        ClassParameter.from(long.class, nanoappId));
+    ReflectionHelpers.callInstanceMethod(
+        NanoAppInstanceInfo.class,
+        instanceInfo,
+        "setAppVersion",
+        ClassParameter.from(int.class, nanoappVersion));
+    return instanceInfo;
   }
 
   /**
@@ -40,17 +97,82 @@ public class ShadowContextHubManager {
 
   @Implementation(minSdk = VERSION_CODES.P)
   @HiddenApi
-  protected ContextHubClient createClient(
-      ContextHubInfo contextHubInfo, ContextHubClientCallback contextHubClientCallback) {
+  protected Object /* ContextHubClient */ createClient(
+      Object /* ContextHubInfo */ contextHubInfo,
+      Object /* ContextHubClientCallback */ contextHubClientCallback) {
     return ReflectionHelpers.newInstance(ContextHubClient.class);
   }
 
   @Implementation(minSdk = VERSION_CODES.P)
   @HiddenApi
-  protected ContextHubClient createClient(
-      ContextHubInfo contextHubInfo,
-      ContextHubClientCallback contextHubClientCallback,
-      Executor executor) {
+  protected Object /* ContextHubClient */ createClient(
+      Object /* ContextHubInfo */ contextHubInfo,
+      Object /* ContextHubClientCallback */ contextHubClientCallback,
+      Object /* Executor */ executor) {
     return ReflectionHelpers.newInstance(ContextHubClient.class);
+  }
+
+  @Implementation(minSdk = VERSION_CODES.P)
+  @HiddenApi
+  protected Object queryNanoApps(ContextHubInfo hubInfo) {
+    @SuppressWarnings("unchecked")
+    ContextHubTransaction<List<NanoAppState>> transaction =
+        ReflectionHelpers.callConstructor(
+            ContextHubTransaction.class,
+            ClassParameter.from(int.class, ContextHubTransaction.TYPE_QUERY_NANOAPPS));
+    Set<Integer> uids = contextHubToNanoappUid.get(hubInfo);
+    List<NanoAppState> nanoappStates = new ArrayList<>();
+    if (uids == null) {
+      uids = new HashSet<>();
+    }
+
+    for (Integer uid : uids) {
+      NanoAppInstanceInfo info = nanoappUidToInfo.get(uid);
+      if (info == null) {
+        continue;
+      }
+      nanoappStates.add(
+          new NanoAppState(info.getAppId(), info.getAppVersion(), true /* enabled */));
+    }
+    @SuppressWarnings("unchecked")
+    ContextHubTransaction.Response<List<NanoAppState>> response =
+        ReflectionHelpers.newInstance(ContextHubTransaction.Response.class);
+    ReflectionHelpers.setField(response, "mResult", ContextHubTransaction.RESULT_SUCCESS);
+    ReflectionHelpers.setField(response, "mContents", nanoappStates);
+    ReflectionHelpers.callInstanceMethod(
+        transaction, "setResponse", ClassParameter.from(response.getClass(), response));
+    return transaction;
+  }
+
+  /**
+   * Provides an array of fake handles.
+   *
+   * <p>These describe an optional physical chip on the device which does not exist during testing.
+   * This implementation enables testing of classes that utilize these APIs.
+   */
+  @Implementation
+  @HiddenApi
+  protected int[] getContextHubHandles() {
+    int[] handles = new int[contextHubInfoList.size()];
+    for (int i = 0; i < handles.length; i++) {
+      handles[i] = i;
+    }
+    return handles;
+  }
+
+  @Implementation
+  @HiddenApi
+  protected ContextHubInfo getContextHubInfo(int hubHandle) {
+    if (hubHandle < 0 || hubHandle >= contextHubInfoList.size()) {
+      return null;
+    }
+
+    return contextHubInfoList.get(hubHandle);
+  }
+
+  @Implementation
+  @HiddenApi
+  protected NanoAppInstanceInfo getNanoAppInstanceInfo(int nanoAppHandle) {
+    return nanoappUidToInfo.get(nanoAppHandle);
   }
 }
